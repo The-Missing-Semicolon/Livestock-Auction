1c2d236651a3606d29c8ae38b90632f8ab7bdfac
John Berkey <jhberkey@gmail.com>
2019-09-11 21:35:13 -0400

    Updates the Total Weight Sold report to use the fair name from settings

--- Livestock Auction/Reports/Forms/frmrptWeightSold.Designer.cs
+++ Livestock Auction/Reports/Forms/frmrptWeightSold.Designer.cs
@@ -29,33 +29,33 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource3 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            this.clsExhibitBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.clsExhibitBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.clsSettingsBindingSource = new System.Windows.Forms.BindingSource(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.clsExhibitBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.clsSettingsBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // clsExhibitBindingSource
-            // 
-            this.clsExhibitBindingSource.DataSource = typeof(Livestock_Auction.DB.clsExhibit);
-            // 
             // reportViewer1
             // 
             this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            reportDataSource2.Name = "Livestock_Auction_clsExhibit";
-            reportDataSource2.Value = this.clsExhibitBindingSource;
-            reportDataSource3.Name = "Livestock_Auction_clsSettings";
-            reportDataSource3.Value = this.clsSettingsBindingSource;
+            reportDataSource1.Name = "Livestock_Auction_clsExhibit";
+            reportDataSource1.Value = this.clsExhibitBindingSource;
+            reportDataSource2.Name = "Livestock_Auction_clsSettings";
+            reportDataSource2.Value = this.clsSettingsBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
             this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource3);
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "Livestock_Auction.Reports.rptWeightSold.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.Size = new System.Drawing.Size(594, 528);
             this.reportViewer1.TabIndex = 0;
+            // 
+            // clsExhibitBindingSource
+            // 
+            this.clsExhibitBindingSource.DataSource = typeof(Livestock_Auction.DB.clsExhibit);
             // 
             // clsSettingsBindingSource
             // 
@@ -68,7 +68,7 @@
             this.ClientSize = new System.Drawing.Size(594, 528);
             this.Controls.Add(this.reportViewer1);
             this.Name = "frmrptWeightSold";
-            this.Text = "frmrptWeightSold";
+            this.Text = "Total Weight Sold Report";
             this.Load += new System.EventHandler(this.frmrptWeightSold_Load);
             ((System.ComponentModel.ISupportInitialize)(this.clsExhibitBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.clsSettingsBindingSource)).EndInit();
